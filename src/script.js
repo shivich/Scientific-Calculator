827344584fa84ded3987358ba0bf53206d1d008e
Shivani Chandra <32413621+shivich@users.noreply.github.com>
2023-04-23 17:49:19 +0000

    functionality added

--- src/script.js
+++ src/script.js
@@ -5,15 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.getElementsByClassName('btn');
 
     let currentValue = "";
+   
 
     // function to evaluate result, But we must not use eval() function as it is risky
     //we are also replacing some operators so that machine can understand
 
     function evaluateResult() {
         console.log('currentValue:', currentValue)
-        const convertedValue = currentValue.replace("×", "*")
+        const convertedValue = currentValue
+            .replace("×", "*")
             .replace("÷", "/")
-            .replace("%", "*0.01");
+            .replace("%", "*0.01")
+            .replace("sin", "Math.sin")
+            .replace("cos", "Math.cos")
+            .replace("ln", "Math.log")
+            .replace("𝝅", "Math.PI")
+            .replace("log", "Math.log10")
+            .replace("e", "Math.E")
+            .replace("tan", "Math.tan")
+            .replace("√", "Math.sqrt")
+            .replace("Exp", "Math.exp")
+            .replace("x^y", "**")
+            .replace("x!" , "factorial")
+            .replace("Inv" , "getInverse");
 
         //we are storing evaluated value in result variable and converting it into String
         console.log('convertedValue:', convertedValue)
@@ -23,28 +37,65 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    function factorial(num) {
+        if (num === 0 || num === 1) {
+          return 1;
+        }
+        else {
+          return num * factorial(num - 1);
+        }
+      }
+
+      function getInverse(num) {
+        // Check if the number is 0
+        if (num === 0) {
+          throw new Error("Cannot find inverse of 0");
+        }
+        
+        // Calculate the inverse
+        const inverse = 1 / num;
+        
+        return inverse;
+      }
+
+    
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
             const value = button.innerText;
 
-            // if AC button is clicked this code will make display empty
+
+            try{
+                 // if AC button is clicked this code will make display empty
             if (value == "AC") {
                 currentValue = "";
                 dispaly.value = currentValue;
             }
 
             // if equal to button is clicked it calls the function eavluateaResult
-            else if(value == "=") {
+            else if (value == "=") {
                 evaluateResult();
             }
 
+
+
             // this code will add value to display when button is clicked
             else {
+                
+                
                 currentValue += value;
-                console.log('currentValue:', currentValue);
+                dispaly.value = currentValue;
+                }
+            }
+
+            catch (error){
+
+                console.error(error);
+                currentValue="ERROR";
                 dispaly.value = currentValue;
             }
+           
+          
         })
     }
 });
